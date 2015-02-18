84e09aa2138619cc9f6288ca15c73b1c16e5d9f8
Dor Gallego <dor.gallego@gmail.com>
2015-02-17 22:32:36 -0500

    Update recursive_factorial.py

--- math/recursive_factorial.py
+++ math/recursive_factorial.py
@@ -1,8 +1,8 @@
 #Alternitavely you can import math and use the factorial method ---> math.factorial(x)
 #This is my recursive implementation of factorials though
 
-def factorial(n):
+def recursive_factorial(n):
   if n == 0:
     return 1
   else:
-    return n * factorial(n-1)
+    return n * recursive_factorial(n-1)
